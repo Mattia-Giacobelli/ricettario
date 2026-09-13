8a777e3e8878506b6256270ec7dc88c905b2eab7
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-09-13 18:56:05 +0200

    fix cors domain

--- src/main/java/com/example/ricettario/config/WebConfig.java
+++ src/main/java/com/example/ricettario/config/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("https://ricettario-spring.duckdns.org",
                 "https://ricettario-react.giacobelli-mattia12.workers.dev", "http://localhost:5173/",
-                "https://www.mattiagiacobellidev.it/ricettario-react"));
+                "https://www.mattiagiacobellidev.it"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // fondamentale per i cookie cross-origin
